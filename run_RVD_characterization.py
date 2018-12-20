495375d3584612c31c434922e1eb41d4ec27b0b0
Matias Senger <m.senger@hotmail.com>
2018-12-20 09:31:03 -0300

    Multi thread is working!

--- run_RVD_characterization.py
+++ run_RVD_characterization.py
@@ -1,12 +1,12 @@
 import threading
 import os
 
-N_PROCESSING_THREADS = 4
+N_PROCESSING_THREADS = 2
 
 def measure(n_runs=1):
 	if not isinstance(n_runs, int) or n_runs < 1:
 		raise TypeError('"n_runs" must be a positive integer number')
-	while n_runs > 0
+	while n_runs > 0:
 		print('Thread: ' + threading.current_thread().getName() + ' --> # of runs left = ' + str(n_runs))
 		os.system("start /wait cmd /c python measure_many_frequencies.py")
 		n_runs -= 1
@@ -15,7 +15,7 @@ def measure(n_runs=1):
 def process_data():
 	os.system("start /wait cmd /c python while_true_process_data.py")
 
-measuring_thread = threading.Thread(target=measure, name='measuring_thread', args=2)
+measuring_thread = threading.Thread(target=measure, name='measuring_thread')
 processing_threads = []
 for k in range(N_PROCESSING_THREADS):
 	processing_threads.append(threading.Thread(target=process_data, name='process_data_thread#'+str(k)))
